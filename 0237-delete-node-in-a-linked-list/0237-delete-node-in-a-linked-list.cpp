666b0ba9847831db6788887d18b20f5c19cbc61c
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-02-20 22:57:04 +0530

    Time: 0 ms (100%), Space: 12.5 MB (17.98%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,15 +9,23 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* temp = node;
-        while (temp->next->next != NULL) {
-            int data = temp->val;
-            temp->val = temp->next->val;
-            temp->next->val = data;
-            temp = temp->next;
-        }
-        temp->val = temp->next->val;
-        delete temp->next;
-        temp->next = NULL;
+        // ListNode* temp = node;
+        // while (temp->next->next != NULL) {
+        //     int data = temp->val;
+        //     temp->val = temp->next->val;
+        //     temp->next->val = data;
+        //     temp = temp->next;
+        // }
+        // temp->val = temp->next->val;
+        // delete temp->next;
+        // temp->next = NULL;
+
+        // Optimised approach below:
+        // if (node == nullptr || node->next == nullptr) return; // Optional => Edge case: last node can't be deleted this way
+
+        node->val = node->next->val; 
+        ListNode* temp = node->next; 
+        node->next = node->next->next; 
+        delete temp; 
     }
 }; 
