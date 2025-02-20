abe3b276b964f9724244a46391deb84b0252499e
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-02-20 22:56:20 +0530

    Time: 10 ms (32.37%), Space: 12.4 MB (52.88%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -10,14 +10,14 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         ListNode* temp = node;
-        while (temp->next->next != NULL) { // Stop at second last node
+        while (temp->next->next != NULL) {
             int data = temp->val;
             temp->val = temp->next->val;
             temp->next->val = data;
             temp = temp->next;
         }
-        temp->val = temp->next->val; // Copy last node value to second last node
-        delete temp->next; // Delete last node
-        temp->next = NULL; // Set next pointer to NULL
+        temp->val = temp->next->val;
+        delete temp->next;
+        temp->next = NULL;
     }
 }; 
