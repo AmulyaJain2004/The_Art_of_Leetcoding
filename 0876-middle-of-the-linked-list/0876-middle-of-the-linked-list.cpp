41ae2fb45f54a6d7f55cf3d64dc287ec569fb160
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-02-22 12:19:17 +0530

    Time: 0 ms (100%), Space: 9.8 MB (89.07%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -12,23 +12,29 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         // Brute force using counting the node first and return the node at (n/2 + 1)th position
-        int cnt = 0;
-        ListNode* temp = head;
-        while (temp != NULL){
-            cnt ++;
-            temp = temp->next;
-        }
-        temp = head;
-        int middleIndex = int(cnt/2)+1;
-        while (middleIndex!=0){
-            middleIndex --;
-            if (middleIndex == 0){
-                break;
-            }
-            temp = temp->next;
-        }
-        return temp;
+        // int cnt = 0;
+        // ListNode* temp = head;
+        // while (temp != NULL){
+        //     cnt ++;
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // int middleIndex = int(cnt/2)+1;
+        // while (middleIndex!=0){
+        //     middleIndex --;
+        //     if (middleIndex == 0){
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+        // return temp;
         // Optimized Approach: Tortoise&Hare approach or Slow&Fast pointers
-
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
