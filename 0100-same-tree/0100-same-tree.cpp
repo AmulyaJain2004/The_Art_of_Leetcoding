4121b10448ecaaab0d0dfb888f7eaa232f59e8b3
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-07-14 21:06:34 +0530

    Time: 0 ms (100%), Space: 12.9 MB (14.82%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,22 +11,30 @@
  */
 class Solution {
 public:
-    void postorder(TreeNode* node, vector<int> &vec){
-        if (node == nullptr) {
-            vec.push_back(INT_MIN);
-            return ;
-        }
-        postorder(node->left, vec);
-        postorder(node->right, vec);
-        vec.push_back(node->val);
-    }
+    // This approach might have worked but it will not work in case of using inorder 
+    // void postorder(TreeNode* node, vector<int> &vec){
+    //     if (node == nullptr) {
+    //         vec.push_back(INT_MIN);
+    //         return ;
+    //     }
+    //     postorder(node->left, vec);
+    //     postorder(node->right, vec);
+    //     vec.push_back(node->val);
+    // }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr) return true;
-        vector<int> vp;
-        vector<int> vq;
-        postorder(p, vp);
-        postorder(q, vq);
-        if (vp == vq) return true;
-        return false;
+        // if (p == nullptr && q == nullptr) return true;
+        // vector<int> vp;
+        // vector<int> vq;
+        // postorder(p, vp);
+        // postorder(q, vq);
+        // if (vp == vq) return true;
+        // return false;
+
+        // Correct Approach
+        if (!p && !q) return true;               // dono null — same
+        if (!p || !q) return false;              // ek null, ek non-null — not same
+        if (p->val != q->val) return false;      // values different — not same
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right); // Checking both trees recursively
     }
 };
