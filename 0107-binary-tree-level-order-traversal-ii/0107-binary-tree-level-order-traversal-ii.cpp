3abaeed1cbac2b1ef957ef67afcb4e3a677a6ed6
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-07-15 22:04:46 +0530

    Time: 1 ms (31.23%), Space: 12.9 MB (99.98%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -32,6 +32,8 @@ public:
         for (int i = 0; i < ans.size()/2 ; i++){
             swap(ans[i],ans[ans.size() - i - 1]);
         }
+        root -> left = NULL;
+        root -> right = NULL;
         return ans;
     }
 };
