16cb772450a650064ced071c5a5a211ec20f3152
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-08-10 00:05:06 +0530

    Time: 8 ms (12.27%), Space: 33.1 MB (38.62%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -21,11 +21,51 @@
  */
 class Solution {
 public:
-    ListNode* findMid(ListNode* head){
-        if (head == nullptr) return nullptr;
-        ListNode* slow = head;
+    // my approach 1:
+    // ListNode* findMid(ListNode* first, ListNode* last){
+    //     ListNode* slow = first;
+    //     ListNode* fast = first;
+    //     while (fast!=last && fast->next!=last){
+    //         slow = slow->next;
+    //         fast = fast->next->next;
+    //     }
+    //     return slow;
+    // }
+    // TreeNode* BST(ListNode* first, ListNode* last){
+    //     if (first == last) return nullptr;
+    //     ListNode* mid = findMid(first, last);
+    //     TreeNode* node = new TreeNode(mid->val);
+    //     node->left = BST(first, mid);
+    //     node->right = BST(mid->next, last);
+    //     return node;
+    // }
+
+    // my approach 2:
+    TreeNode* BST(ListNode* &head, int left, int right) {
+        if (left > right) return nullptr;
+        int mid = left + (right - left) / 2;
+        TreeNode* leftChild = BST(head, left, mid - 1);
+        TreeNode* root = new TreeNode(head->val);
+        head = head->next;
+        TreeNode* rightChild = BST(head, mid + 1, right);
+        root->left = leftChild;
+        root->right = rightChild;
+        return root;
     }
+
     TreeNode* sortedListToBST(ListNode* head) {
-        
+        // my approach 1:
+        // if (head == nullptr) return nullptr;
+        // ListNode* firstNode = head;
+        // return BST(firstNode, nullptr);
+
+        // my approach 2:
+        int cnt = 0;
+        ListNode* temp = head;
+        while(temp != nullptr) {
+            temp = temp->next;
+            cnt++;
+        }
+        return BST(head, 0, cnt-1);
     }
 };
