02b1039c19c914b74311d3e3ed1e59561486a640
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-08-22 00:41:48 +0530

    Time: 0 ms (100%), Space: 17.8 MB (36.25%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
+        // instead of stoi check the last digit for odd/even 
+        // because stoi can overflow for large int
+        // but in below for loop we are already checking from last digit so no need.
         for (int i = num.size() -1; i >=0; i--) {
             if (int(num[i]) % 2 != 0) {
                 return num.substr(0, i+1);
