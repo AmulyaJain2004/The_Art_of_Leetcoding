7d7b8dc0ace191508a8709fb97d792258c9a9838
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-08-24 15:44:23 +0530

    Time: 3 ms (5.6%), Space: 9 MB (10.97%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         for (int j = 0; j < s.size(); j ++) {
-            char c = s[0];
-            for (int i = 1; i < s.size(); i++) {
-                s[i-1] = s[i];
-            }
-            s[s.size()-1] = c;
-            if (s == goal) {
+            // char c = s[0];
+            // for (int i = 1; i < s.size(); i++) {
+            //     s[i-1] = s[i];
+            // }
+            // s[s.size()-1] = c;
+            // if (s == goal) {
+            //     return true;
+            // }
+            // we can do above using substring
+            string temp = s.substr(1) + s[0];
+            if (temp == goal) {
                 return true;
             }
+            s = temp;
         }
         return false;
     }
