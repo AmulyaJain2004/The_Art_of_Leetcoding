a4de2f887da92b1b8656cfe2680023c1857cbc0c
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-08-24 14:25:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,12 +6,8 @@ public:
         }
         unordered_map<char, int> mp1, mp2;
         for (int i = 0 ; i < s.size(); i ++) {
-            if (mp1.find(s[i]) == mp1.end()) {
-                mp1[s[i]] ++;
-            }
-            if (mp2.find(t[i]) == mp2.end()) {
-                mp2[t[i]] ++;
-            }
+            mp1[s[i]] ++;
+            mp2[t[i]] ++;
         }
         if (mp1 == mp2) {
             return true;
