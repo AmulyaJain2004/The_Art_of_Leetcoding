2a62cfa1457dbf79870f9513dd42aa284a5a95b6
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-08-29 15:34:45 +0530

    Time: 0 ms (100%), Space: 8.4 MB (25.79%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> stk;
+        int ans = 0;
+        int localMaxNum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                stk.push(s[i]);
+                if (localMaxNum < stk.size()) {
+                    localMaxNum ++;
+                }
+            }
+            else if (s[i] == ')') {
+                if (ans < stk.size()) {
+                    ans = stk.size();
+                }
+                stk.pop();
+                // if (stk.empty()) {
+                //     ans = localMaxNum;
+                //     localMaxNum = 0;
+                // }
+            }
+        }
+        return ans;
+    }
+};
