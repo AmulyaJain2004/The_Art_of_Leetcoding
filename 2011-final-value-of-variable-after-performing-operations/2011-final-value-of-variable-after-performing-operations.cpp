c973cf3a2e3c27c91b92804d08e44a31193854f1
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-09-16 17:26:42 +0530

    Time: 0 ms (100%), Space: 17.6 MB (43.73%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for (int i = 0; i < operations.size(); i++) {
+            if(operations[i][0] == 'X') {
+                if (operations[i][1] == '+') {
+                    ans++;
+                }
+                else {
+                    ans--;
+                }
+            }
+            else if (operations[i][0] == '+') {
+                ++ans;
+            }
+            else {
+                --ans;
+            }
+        }
+        return ans;
+    }
+};
