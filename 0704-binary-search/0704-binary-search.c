709f6ac32ff57cd370becbc9cc6dd26ffc77d646
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-09-29 22:14:32 +0530

    Time: 31 ms (0.1%), Space: 9.4 MB (11.38%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,13 @@
+int search(int* nums, int numsSize, int target) {
+    int x = -1;
+    int left = 0;
+    int right = numsSize - 1;
+    while(left <= right)
+    {
+        int middle = left + (right - left)/2;
+        if (nums[middle] == target) return middle;
+        if (nums[middle] < target) left = middle + 1;
+        if (nums[middle] > target) right = middle - 1;
+    }
+    return x;
+}
