da725116749da95a91c1f2f426cd789a609567aa
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-11-17 15:47:53 +0530

    Time: 0 ms (100%), Space: 61.2 MB (97.09%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        vector<int> positions;
-        for (int i = 0 ; i < nums.size(); i ++) {
+        // vector<int> positions;
+        // for (int i = 0 ; i < nums.size(); i ++) {
+        //     if (nums[i] == 1) {
+        //         positions.push_back(i);
+        //     }
+        // }
+        // bool is_klen = true;
+        // int i = 0;
+        // int j = 1;
+        // while (j < positions.size()) {
+        //     if (positions[j] - positions[i] - 1 >= k) {
+        //         is_klen = is_klen && true;
+        //     }
+        //     else {
+        //         is_klen = is_klen && false;
+        //     }
+        //     i++;
+        //     j++;
+        // }
+        // return is_klen;
+
+        bool is_klen = true;
+        int count = 0;
+        int i = 0;
+        for (; i < nums.size(); i ++) {
             if (nums[i] == 1) {
-                positions.push_back(i);
+                break;
             }
         }
-        bool is_klen = true;
-        int i = 0;
-        int j = 1;
-        while (j < positions.size()) {
-            if (positions[j] - positions[i] - 1 >= k) {
-                is_klen = is_klen && true;
+        for (int j = i+1; j < nums.size(); j++) {
+            if (nums[j] == 0) {
+                count ++;
             }
             else {
-                is_klen = is_klen && false;
+                if (count >= k) {
+                    is_klen = is_klen && true;
+                }
+                else {
+                    is_klen = is_klen && false;
+                }
+                count = 0;
             }
-            i++;
-            j++;
         }
         return is_klen;
     }
