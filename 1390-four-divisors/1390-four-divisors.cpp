b422a375602df39c41ea9e1a2245f3ff358f62f3
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-01-05 00:28:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int sumdivisors = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            vector<int> divisors;
+            for (int j = 1; j*j < nums[i]; j++) {
+                if(nums[i] % j == 0) {
+                    divisors.push_back(j);
+                    if (j != nums[i] / j) {
+                        divisors.push_back(nums[i] / j );
+                    }
+                }
+                if (divisors.size() > 4) {
+                    break;
+                }
+            }
+            if (divisors.size() == 4) {
+                for (int k = 0; k < divisors.size(); k++) {
+                    sumdivisors += divisors[k];
+                }
+            }
+        }
+        return sumdivisors;
+    }
+};
