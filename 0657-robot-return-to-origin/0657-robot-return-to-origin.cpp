e7ad11dca7a2d4bf5ea37a0021a687bbdf0ea25c
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-04-25 20:56:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
         int up = 0;
-        int down = 0;
+        // int down = 0;
         int left = 0;
-        int right = 0;
+        // int right = 0;
         for (int i = 0; i < moves.size(); i++) {
             if (moves[i] == 'L') {
                 left ++;
             }
             else if (moves[i] == 'R') {
-                right ++;
+                // right ++;
+                left --;
             }
             else if (moves[i] == 'U') {
                 up ++;
             }
             else {
-                down ++;
+                // down ++;
+                up --;
             }
         }
-        if (up - down == 0 && left - right == 0) {
+        // if (up - down == 0 && left - right == 0) {
+        if (up == 0 && left == 0) {
             return true;
         }
         return false;
