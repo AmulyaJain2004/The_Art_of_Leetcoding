4916ed6314863a8dc0038e74df731d0f4ba46ae6
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-04-25 20:54:41 +0530

    Time: 4 ms (25.87%), Space: 10.3 MB (45.72%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int up = 0;
+        int down = 0;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < moves.size(); i++) {
+            if (moves[i] == 'L') {
+                left ++;
+            }
+            else if (moves[i] == 'R') {
+                right ++;
+            }
+            else if (moves[i] == 'U') {
+                up ++;
+            }
+            else {
+                down ++;
+            }
+        }
+        if (up - down == 0 && left - right == 0) {
+            return true;
+        }
+        return false;
+    }
+};
