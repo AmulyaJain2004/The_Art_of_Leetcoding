bbbee2b3baafac438ab1b9321aa4153376fcc32c
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-05-20 14:07:30 +0530

    Time: 0 ms (100%), Space: 85.7 MB (85.47%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -2,18 +2,39 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        vector <int> ans;
-        unordered_map <int, int> mp;
+        // first solution
+        // vector <int> ans;
+        // unordered_map <int, int> mp;
+        // for (int i = 0; i < n; i++) {
+        //     mp[A[i]]++;
+        //     mp[B[i]]++;
+        //     int cnt = 0;
+        //     for (auto it: mp) {
+        //         if (it.second == 2) {
+        //             cnt ++;
+        //         }
+        //     }
+        //     ans.push_back(cnt);
+        // }
+        // return ans;
+        
+        // optimized solution
+        // what we will do?
+        // we have to do store frequency of element in array instead of maintaining a map
+        // since every element is distinct, we can store frequency of element at index = element only
+        vector <int> ans(n);
+        vector <int> frequency(n+1, 0);
+        int common = 0;
         for (int i = 0; i < n; i++) {
-            mp[A[i]]++;
-            mp[B[i]]++;
-            int cnt = 0;
-            for (auto it: mp) {
-                if (it.second == 2) {
-                    cnt ++;
-                }
+            frequency[A[i]] ++;
+            if (frequency[A[i]] == 2) {
+                common ++;
             }
-            ans.push_back(cnt);
+            frequency[B[i]] ++;
+            if (frequency[B[i]] == 2) {
+                common ++;
+            }
+            ans[i] = common;
         }
         return ans;
     }
