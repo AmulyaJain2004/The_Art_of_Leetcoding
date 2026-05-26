d594b11ad0d22c9809158ac6433d9c3c00ea136b
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-05-26 18:24:47 +0530

    Time: 0 ms (100%), Space: 9.7 MB (19.49%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int cnt = 0;
+        unordered_set<char> upperset;
+        unordered_set<char> lowerset;
+        for (int i = 0; i < word.size(); i++) {
+            if (isupper(word[i])) {
+                upperset.insert(word[i]);
+            }
+            else {
+                lowerset.insert(word[i]);
+            }
+        }
+        for (char c : lowerset) {
+            if (upperset.find(c - 32) != upperset.end()) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
