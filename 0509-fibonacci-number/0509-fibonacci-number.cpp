f6f02b022cc59ea52fa1547b77230ac01eeb96a1
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-07-11 16:54:15 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp) {
-        if (n <= 1) {
-            return n;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    }
+    // int solve(int n, vector<int> &dp) {
+    //     if (n <= 1) {
+    //         return n;
+    //     }
+    //     if (dp[n] != -1) {
+    //         return dp[n];
+    //     }
+    //     return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    // }
     int fib(int n) {
         if (n <= 1) { // lowest possible value
             return n;
         }
-        vector <int> dp(n+1, -1);
-        return solve(n, dp);
+        // vector <int> dp(n+1, -1); // dp[i] will represent ith fibonacci number
+        // return solve(n, dp);
+
+        // bottom up
+        int a = 0;
+        int b = 1;
+        int c;
+        for (int i = 1; i < n; i++) {
+            c = a + b;
+            a = b; 
+            b = c;
+        }
+        return c;
     }
 };
