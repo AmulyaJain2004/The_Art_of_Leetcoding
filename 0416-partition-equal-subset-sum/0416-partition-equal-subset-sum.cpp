b2132dd5af0536fac0cb3d0ca2bd5fc81fbf601e
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-08-19 03:25:02 +0530

    Time: 187 ms (14.25%), Space: 17.6 MB (55.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -11,25 +11,19 @@ public:
         }
         int target = totalSum / 2;
         vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-        // Sum 0 is always possible: choose nothing
         for (int i = 0; i <= n; i++) {
             dp[i][0] = true;
         }
         for (int i = 1; i <= n; i++) {
+            int x = 0;
             for (int j = 1; j <= target; j++) {
-
-                // Don't take nums[i-1]
-                dp[i][j] = dp[i - 1][j];
-
-                // Take nums[i-1]
                 if (nums[i - 1] <= j) {
-                    dp[i][j] =
-                        dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                    x = dp[i - 1][j - nums[i - 1]];
                 }
+                int y = dp[i - 1][j];
+                dp[i][j] = x || y;
             }
         }
-
         return dp[n][target];
-
     }
 };
