0d7b9ff04c74feb410bf7d04121bfa94f8c3baee
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-08-25 10:51:41 +0530

    Time: 0 ms (100%), Space: 23.9 MB (94.59%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -9,17 +9,25 @@ public:
         // }
         // return k*(nums.size() + 1);
 
-        vector <bool> arr(101, false);
-        for (int i  = 0; i < nums.size(); i++) {
-            if (nums[i] % k == 0) {
-                arr[nums[i]/k] = true;
+        // vector <bool> arr(101, false);
+        // for (int i  = 0; i < nums.size(); i++) {
+        //     if (nums[i] % k == 0) {
+        //         arr[nums[i]/k] = true;
+        //     }
+        // }
+        // for (int i = 1; i <= 100; i ++) {
+        //     if(arr[i] == false) {
+        //         return k * i;
+        //     }
+        // }
+        // return k*101;
+
+        sort(nums.begin(), nums.end());
+        for (int i = k;; i += k) {
+            if (!binary_search(nums.begin(), nums.end(), i)) {
+                return i;
             }
         }
-        for (int i = 1; i <= 100; i ++) {
-            if(arr[i] == false) {
-                return k * i;
-            }
-        }
-        return k*101;
+        return -1;
     }
 };
