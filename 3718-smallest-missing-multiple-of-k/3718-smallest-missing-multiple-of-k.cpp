80b1f995bee694cb1e708692d5894080bbdc6ed9
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-08-25 10:44:10 +0530

    Time: 0 ms (100%), Space: 24 MB (94.59%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> st (nums.begin(), nums.end());
-        for (int i = 1; i <= nums.size(); i ++) {
-            if (st.find(k*i) == st.end()) {
-                return k*i;
+        // unordered_set<int> st (nums.begin(), nums.end());
+        // for (int i = 1; i <= nums.size(); i ++) {
+        //     if (st.find(k*i) == st.end()) {
+        //         return k*i;
+        //     }
+        // }
+        // return k*(nums.size() + 1);
+
+        vector <bool> arr(101, false);
+        for (int i  = 0; i < nums.size(); i++) {
+            if (nums[i] % k == 0) {
+                arr[nums[i]/k] = true;
             }
         }
-        return k*(nums.size() + 1);
+        for (int i = 1; i <= 100; i ++) {
+            if(arr[i] == false) {
+                return k * i;
+            }
+        }
+        return k*101;
     }
 };
