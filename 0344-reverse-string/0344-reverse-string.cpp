49e6667bdd642a291f33b4b11cb2ca3f13484238
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-08-25 17:54:08 +0530

    Time: 35 ms (2.07%), Space: 29.6 MB (5.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int start = 0;
-        int end = s.size() - 1;
-        while (start < end) {
-            swap (s[start], s[end]);
-            start ++;
-            end --;
+    void reverse(vector<char>& s, int n) {
+        if (n == 1) {
+            return;
         }
+        char c = s[0];
+        s.erase(s.begin());
+        reverse(s, n-1);
+        s.push_back(c);
+    }
+    void reverseString(vector<char>& s) {
+        // iterative and two pointer 
+        // int start = 0;
+        // int end = s.size() - 1;
+        // while (start < end) {
+        //     swap (s[start], s[end]);
+        //     start ++;
+        //     end --;
+        // }
+
+        // recursive
+        int n = s.size();
+        reverse(s, n);
     }
 };
